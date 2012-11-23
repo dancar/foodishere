a56536e18dc26dab4c40fe3f2c642ac83f120f3c
dan <dan@W500.(none)>
2012-11-23 15:44:07 +0200

    User not found now redirects to login

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -1,14 +1,15 @@
 module AuthenticationHelper
   def signed_in?
-    !session[:user_id].nil?
+    !session[:user_id].nil? && current_user
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id]) rescue nil
   end
 
   def ensure_signed_in
     unless signed_in?
+      session[:user_id] = nil
       session[:redirect_to] = request.url
       redirect_to(new_session_path)
     end
