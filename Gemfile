babcbd21b3b76efbd53030a504e29536ac38d312
Dan Carmon <dan@carmon.org.il>
2013-04-01 18:48:35 +0300

    Introduced fetch_orders

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem "mechanize"
 gem "settingslogic"
 gem "ruby-openid"
 gem "rack-openid"
---
+++ lib/fetch_orders.rb
@@ -0,0 +1,17 @@
+require 'mechanize'
+
+module FoodIsHere
+  def self.fetch_orders
+    agent = Mechanize.new
+    login_page = agent.get("http://www.couponphone.co.il")
+    login_form = login_page.forms.first
+    login_form["ctl00$txbUserName"] = ENV["CP_UN"]
+    login_form["ctl00$txbPassword"] = ENV["CP_PW"]
+    login_button = login_form.button_with(value: "OK")
+    login_form.submit(login_button)
+    agent.get("http://www.couponphone.co.il/OrderStatus/")
+    sent_orders_page = agent.get("http://www.couponphone.co.il/OrderStatus/LoadSentOrders.aspx")
+    rests_ids = sent_orders_page.forms.first.fields_with(id: "hdfRestaurantID").map{|field| field.value}
+    rests = Restaurant.where(:cp_id => rests_ids)
+  end
+end
